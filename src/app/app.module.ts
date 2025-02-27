076115fc25be9a34ec08cf0457904104bc9fa577
Jose Horacio <jhoracio19@hotmail.com>
2025-02-27 01:06:55 -0600

    registro-admin terminado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,12 +12,21 @@ import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
 import {MatDividerModule} from '@angular/material/divider';
 import {MatRadioModule} from '@angular/material/radio';
+import {MatInputModule} from '@angular/material/input';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+
 import { RegistroUsuariosScreenComponent } from './screens/registro-usuarios-screen/registro-usuarios-screen.component';
 import { HomeScreenComponent } from './screens/home-screen/home-screen.component';
 import { NavbarComponent } from './partials/navbar/navbar.component';
 import { RegistroAdminComponent } from './partials/registro-admin/registro-admin.component';
 import { RegistroAlumnosComponent } from './partials/registro-alumnos/registro-alumnos.component';
 import { RegistroMaestrosComponent } from './partials/registro-maestros/registro-maestros.component';
+
+//Para usar el mask
+import { NgxMaskDirective, provideNgxMask } from 'ngx-mask';
+
+// Cambia al idioma español
+import { MAT_DATE_LOCALE } from '@angular/material/core';
 
 @NgModule({
   declarations: [
@@ -39,8 +48,14 @@ import { RegistroMaestrosComponent } from './partials/registro-maestros/registro
     MatDividerModule,
     FormsModule,
     MatRadioModule,
+    MatInputModule,
+    NgxMaskDirective,
+    MatDatepickerModule,
   ],
-  providers: [],
+  providers: [
+    {provide: MAT_DATE_LOCALE, useValue: 'es-ES'},
+    provideNgxMask(),
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
