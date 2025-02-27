6ad3b8d73042bbd88b388d960d901f5bee4e49f0
Jose Horacio <jhoracio19@hotmail.com>
2025-02-27 00:04:15 -0600

    navbar terminado

--- src/app/partials/navbar/navbar.component.ts
+++ src/app/partials/navbar/navbar.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
+  @Input() tipo: string = "";
+  @Input() rol:string ="";
 
+  public token : string = "";
+  public editar:boolean = false;
+
+  constructor(){}
+
+  ngOnInit(): void {
+    
+  }
+
+  public logout(){
+
+  }
 }
--- src/app/screens/registro-usuarios-screen/registro-usuarios-screen.component.ts
+++ src/app/screens/registro-usuarios-screen/registro-usuarios-screen.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-registro-usuarios-screen',
   templateUrl: './registro-usuarios-screen.component.html',
   styleUrls: ['./registro-usuarios-screen.component.scss']
 })
-export class RegistroUsuariosScreenComponent {
+export class RegistroUsuariosScreenComponent implements OnInit{
+
+  public tipo:string = "registro-usuarios";
+  public user:any = {};
+  public editar:boolean = false;
+  //Banderas para el tipo de usuario
+  public isAdmin:boolean = false;
+  public isAlumno:boolean = false;
+  public isMaestro:boolean = false;
+
+  public tipo_user:string = "";
+
+  constructor(){}
+
+  ngOnInit(): void {
+    
+  }
+
 
 }
